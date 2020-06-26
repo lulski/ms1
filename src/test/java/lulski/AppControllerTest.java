8d512cf0e9da3435cb15016bdc60c97d7e15d8d6
Ichsan Siswoputranto <milunes@gmail.com>
2020-06-26 18:11:26 +1000

    that failed interview :'(

--- src/test/java/lulski/AppControllerTest.java
+++ src/test/java/lulski/AppControllerTest.java
@@ -1,11 +1,16 @@
 package lulski;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,6 +27,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import lulski.model.NavigationMenuItem;
+import lulski.service.RateLimiterService;
 import lulski.service.WebsiteService;
 
 @RunWith(SpringRunner.class)
@@ -32,7 +38,10 @@ public class AppControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @Autowired
+    private RateLimiterService rateLimiterService;
+
+    @Autowired
     private WebsiteService websiteService;
 
     @Test
@@ -43,23 +52,53 @@ public class AppControllerTest {
 
     @Test
     public void getAsyncMethod() throws Exception {
+        // System.out.println(mvc.perform(MockMvcRequestBuilders.get("/asyncmethod")));
+
         mvc.perform(MockMvcRequestBuilders.get("/asyncmethod").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void getNavigationMenuItems() throws Exception{
+    public void getNavigationMenuItems() throws Exception {
         List<NavigationMenuItem> navMenuItems = new ArrayList<NavigationMenuItem>();
-        navMenuItems.add(new NavigationMenuItem("home","/home"));
-        navMenuItems.add(new NavigationMenuItem("home","/pics"));
-        navMenuItems.add(new NavigationMenuItem("home","/notes"));
+        navMenuItems.add(new NavigationMenuItem("home", "/home"));
+        navMenuItems.add(new NavigationMenuItem("home", "/pics"));
+        navMenuItems.add(new NavigationMenuItem("home", "/notes"));
 
-        ResponseEntity<List<NavigationMenuItem>> expected = new ResponseEntity<List<NavigationMenuItem>>(navMenuItems,HttpStatus.OK);
+        ResponseEntity<List<NavigationMenuItem>> expected = new ResponseEntity<List<NavigationMenuItem>>(navMenuItems,
+                HttpStatus.OK);
 
         when(websiteService.getNavigationMenuItems()).thenReturn(navMenuItems);
 
-
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-        .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+
+    @Test
+    public void testArray() throws Exception {
+
+        ArrayList<String[]> games = new ArrayList<String[]>();
+        String[] ps4Games = { "last of us 2", "uncharted 4", "gran turismo sport" };
+        String[] pcGames = { "dirt2", "valorant", "resident evil 3" };
+
+        games.add(ps4Games);
+        games.add(pcGames);
+
+        for (String[] gamecollection : games) {
+            System.out.println(Arrays.toString(gamecollection));
+        }
+
+        assertArrayEquals(new String[] { "last of us 2", "uncharted 4", "gran turismo sport" }, games.get(0));
+    }
+
+    @Test
+    public void testRateLimiter() throws Exception {
+        System.out.println("rateLimiterService");
+        //when(rateLimiterService.isAllowed("ps1")).thenReturn(false);
+        assertTrue(rateLimiterService.isAllowed("ps1"));
+        assertTrue(rateLimiterService.isAllowed("ps1"));
+
+        System.out.println("rateLimiterService out");
+
     }
 }
