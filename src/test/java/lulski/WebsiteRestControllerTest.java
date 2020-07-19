3dbc5d35e9daad962f4b2210323e6b670da12923
Ichsan Siswoputranto <milunes@gmail.com>
2020-07-19 22:27:54 +1000

    Getting started with RestAssured testing
    + PACT test of /api/website/getnavmenu response

--- src/test/java/lulski/WebsiteRestControllerTest.java
+++ src/test/java/lulski/WebsiteRestControllerTest.java
@@ -1,5 +1,7 @@
 package lulski;
 
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,19 +11,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
 public class WebsiteRestControllerTest {
 
     @LocalServerPort
-	private int port;
+    private int port;
+
+
 
     @Test
     public void website_api_returns_navigation_menu_items(){
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
 
-        RestAssured.when().get("/api/website/getnavmenu").then().statusCode(200);
+
+        RestAssured.when().get("/api/website/getnavmenu").then()
+        .statusCode(HttpStatus.SC_OK)
+        .body(matchesJsonSchemaInClasspath("getnavmenu.json"));
+
 
     }
 }
